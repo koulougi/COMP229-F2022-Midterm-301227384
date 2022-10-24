59422a8e2dcbfacf093901d8533936bbc0913abc
koulougi <112962767+koulougi@users.noreply.github.com>
2022-10-24 16:10:51 -0400

    initializing of repo

---
+++ server/config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+  //local MongoDB deployment ->
+  URI: "mongodb+srv://ckoul:uG8DUhfFemTdMZdU@productstore.xfxwlew.mongodb.net/?retryWrites=true&w=majority",
+};
---
+++ server/routes/products.js
@@ -0,0 +1,59 @@
+// modules required for routing
+let express = require("express");
+let router = express.Router();
+let mongoose = require("mongoose");
+
+// define the product model
+let product = require("../models/products");
+
+/* GET products List page. READ */
+router.get("/", (req, res, next) => {
+  // find all products in the products collection
+  product.find((err, products) => {
+    if (err) {
+      return console.error(err);
+    } else {
+      res.render("products/index", {
+        title: "Products",
+        products: products,
+      });
+    }
+  });
+});
+
+//  GET the Product Details page in order to add a new Product
+router.get("/add", (req, res, next) => {
+  /*****************
+   * ADD CODE HERE *
+   *****************/
+});
+
+// POST process the Product Details page and create a new Product - CREATE
+router.post("/add", (req, res, next) => {
+  /*****************
+   * ADD CODE HERE *
+   *****************/
+});
+
+// GET the Product Details page in order to edit an existing Product
+router.get("/:id", (req, res, next) => {
+  /*****************
+   * ADD CODE HERE *
+   *****************/
+});
+
+// POST - process the information passed from the details form and update the document
+router.post("/:id", (req, res, next) => {
+  /*****************
+   * ADD CODE HERE *
+   *****************/
+});
+
+// GET - process the delete
+router.get("/delete", (req, res, next) => {
+  /*****************
+   * ADD CODE HERE *
+   *****************/
+});
+
+module.exports = router;
